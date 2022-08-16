92a8cea9ae532bc7d00c54629b91215045670a7c
Amrutha <109643705+amrutha-077@users.noreply.github.com>
2022-08-16 12:24:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './GL_Assesment_2/app';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <>
+ <BrowserRouter>
+ <App></App>
+ </BrowserRouter>
+ 
+</>
+ 
+);
+
+
+
+
